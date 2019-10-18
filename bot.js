7824e3f01896a3c9f856a6a48702ffbc375e9ec5
aldolime900 <56601264+aldolime900@users.noreply.github.com>
2019-10-18 15:14:27 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -23,7 +23,7 @@ client.on('ready', () => {
       console.log(`ON ${client.guilds.size} Servers '     Script By : i1Suhaib ' `);
     console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`S2ud`,"http://twitch.tv/S-F")
+client.user.setGame(`محمدالعراقي`,)
 client.user.setStatus("dnd")
 });
 
